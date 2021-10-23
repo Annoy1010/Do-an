f3ff8d4729bc4cec1c43efd9a1a971240c2ceeca
Lê Phúc <lethephuc2002@gmail.com>
2021-10-23 15:27:24 +0700

    phuc

---
+++ DangKy/DangKy/DAO/DataProvider.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangKy.DAO
+{
+    class DataProvider
+    {
+        private static DataProvider instance;
+        private string connectionStr = "Data Source=DESKTOP-0E1NOTR;Initial Catalog=SignUp;Integrated Security=True";
+
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new DataProvider();
+                return DataProvider.instance;
+            }
+            private set
+            {
+                DataProvider.instance = value;
+            }
+        }
+
+        private DataProvider() { }
+
+        public int ExecuteInsertUserQuery(object[] parameter = null)
+        {
+            int rowCount = 0;
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                try
+                {
+                    string query = "INSERT INTO UserAccount VALUES (@userDB,@passDB,@nameDB,@ageDB,@passportDB,@phoneDB,@borrowDB,@adminDB)";
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        switch (i)
+                        {
+                            case 0:
+                                command.Parameters.AddWithValue("@userDB", parameter[i]);
+                                break;
+                            case 1:
+                                command.Parameters.AddWithValue("@passDB", parameter[i]);
+                                break;
+                            case 2:
+                                command.Parameters.AddWithValue("@nameDB", parameter[i]);
+                                break;
+                            case 3:
+                                command.Parameters.AddWithValue("@ageDB", parameter[i]);
+                                break;
+                            case 4:
+                                command.Parameters.AddWithValue("@passportDB", parameter[i]);
+                                break;
+                            case 5:
+                                command.Parameters.AddWithValue("@phoneDB", parameter[i]);
+                                break;
+                            case 6:
+                                command.Parameters.AddWithValue("@borrowDB", parameter[i]);
+                                break;
+                            case 7:
+                                command.Parameters.AddWithValue("@adminDB", parameter[i]);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    rowCount = command.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e);
+                    Console.WriteLine(e.StackTrace);
+                }
+                finally
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+                Console.Read();
+            }
+            return rowCount;
+        }
+
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (var item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                        }
+                    }
+                }
+                data = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return data;
+        }
+
+    }
+}
---
+++ DangKy/DangKy/DangKy.cs
@@ -0,0 +1,99 @@
+﻿using DangKy.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DangKy
+{
+    public partial class DangKy : Form
+    {
+        public DangKy()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+            string repass = textBox3.Text;
+            string fullname = textBox4.Text;
+            string age = textBox5.Text;
+            string passport = textBox6.Text;
+            string phone = textBox7.Text;
+            if (IsNullData(user, pass, repass, fullname, age, passport, phone))
+                MessageBox.Show("Nhập không đủ thông tin. Vui lòng kiểm tra lại.");
+            else
+            {
+                if (!SubmitPassCorrect(pass, repass))
+                    MessageBox.Show("Mật khẩu xác nhận không chính xác. Vui lòng kiểm tra lại.");
+                else
+                {
+                    if (!IsValidNumber(age))
+                        MessageBox.Show("Thông tin 'Tuổi' không chính xác. Vui lòng kiểm tra lại.");
+                    else if (!IsValidNumber(passport))
+                        MessageBox.Show("Thông tin 'Chứng minh thư' không chính xác. Vui lòng kiểm tra lại.");
+                    else if (!IsValidNumber(phone))
+                        MessageBox.Show("Thông tin 'Số điện thoại' không chính xác. Vui lòng kiểm tra lại.");
+                    else
+                    {
+                        if (CheckUserExist(user, pass, fullname, age, passport, phone) == 0)
+                            MessageBox.Show("Tên đăng nhập đã tồn tại. Vui lòng kiểm tra lại.");
+                        else
+                        {
+                            textBox1.Clear();
+                            textBox2.Clear();
+                            textBox3.Clear();
+                            textBox4.Clear();
+                            textBox5.Clear();
+                            textBox6.Clear();
+                            textBox7.Clear();
+                        }
+                    }
+                }
+            }
+        }
+        bool IsNullData(string user, string pass, string repass, string fullname, string age, string passport, string phone)
+        {
+            if (String.IsNullOrEmpty(user) || String.IsNullOrEmpty(pass) || String.IsNullOrEmpty(repass) || String.IsNullOrEmpty(fullname) || String.IsNullOrEmpty(age) || String.IsNullOrEmpty(passport) || String.IsNullOrEmpty(phone))
+                return true;
+            return false;
+        }
+
+        bool IsValidNumber(string str)
+        {
+            if (str.Trim() == "")
+                return false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!char.IsNumber(str[i]))
+                    return false;
+            }
+            if (Int64.Parse(str) < 0)
+                return false;
+            return true;
+        }
+
+        bool SubmitPassCorrect(string pass, string repass)
+        {
+            return repass == pass;
+        }
+
+        int CheckUserExist(string user, string pass, string fullname, string age, string passport, string phone)
+        {
+            int SuccessRows = DataProvider.Instance.ExecuteInsertUserQuery(new object[] { user, pass, fullname, age, passport, phone, 1, 0 });
+            return SuccessRows;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
