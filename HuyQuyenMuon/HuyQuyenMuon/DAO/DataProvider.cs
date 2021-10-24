3669139b2ed7f48730ed64dcdb00764f6c1bdbf4
Lê Phúc <lethephuc2002@gmail.com>
2021-10-24 14:38:01 +0700

    phuc

---
+++ HuyQuyenMuon/HuyQuyenMuon/DAO/DataProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuyQuyenMuon.DAO
+{
+    class DataProvider
+    {
+        private static DataProvider instance;
+        private string connectionStr = "Data Source=DESKTOP-0E1NOTR;Initial Catalog=Library;Integrated Security=True";
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new DataProvider();
+                return DataProvider.instance;
+            }
+            private set
+            {
+                DataProvider.instance = value;
+            }
+        }
+        private DataProvider() { }
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+    }
+}
---
+++ HuyQuyenMuon/HuyQuyenMuon/Form1.cs
@@ -0,0 +1,32 @@
+﻿using HuyQuyenMuon.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HuyQuyenMuon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            object usernameData = dataGridView2.SelectedCells[0].Value;
+            string username = usernameData.ToString();
+            RemoveBorrow(username);
+        }
+        void RemoveBorrow(string username)
+        {
+            string query = "UPDATE UserAccount SET QUYENMUON = 1 WHERE TENDN = '" + username + "'";
+            DataProvider.Instance.ExecuteQuery(query);
+        }
+    }
+}
