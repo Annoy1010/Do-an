0443fd1077ebe4acb58757d796f30dee3b715b89
Lê Phúc <lethephuc2002@gmail.com>
2021-11-07 11:18:20 +0700

    phuc

--- DangKy/DangKy/DangKy.Designer.cs
+++ DangKy/DangKy/DangKy.Designer.cs
@@ -29,6 +29,7 @@ namespace DangKy
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DangKy));
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -49,6 +50,8 @@ namespace DangKy
             this.button2 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
+            this.hidepassword = new System.Windows.Forms.Button();
+            this.hiderepassword = new System.Windows.Forms.Button();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
@@ -57,9 +60,10 @@ namespace DangKy
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnCount = 3;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 26.41315F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73.58685F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 152F));
             this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.label4, 0, 2);
@@ -74,12 +78,14 @@ namespace DangKy
             this.tableLayoutPanel2.Controls.Add(this.textBox5, 1, 4);
             this.tableLayoutPanel2.Controls.Add(this.textBox6, 1, 5);
             this.tableLayoutPanel2.Controls.Add(this.textBox7, 1, 6);
+            this.tableLayoutPanel2.Controls.Add(this.hidepassword, 2, 1);
+            this.tableLayoutPanel2.Controls.Add(this.hiderepassword, 2, 2);
             this.tableLayoutPanel2.Location = new System.Drawing.Point(74, 109);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 7;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.23529F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.19608F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
@@ -91,10 +97,10 @@ namespace DangKy
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 14);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 16);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(175, 29);
+            this.label2.Size = new System.Drawing.Size(145, 25);
             this.label2.TabIndex = 0;
             this.label2.Text = "Tên đăng nhập";
             // 
@@ -102,10 +108,10 @@ namespace DangKy
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(3, 72);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 29);
+            this.label3.Size = new System.Drawing.Size(93, 25);
             this.label3.TabIndex = 1;
             this.label3.Text = "Mật khẩu";
             // 
@@ -113,10 +119,10 @@ namespace DangKy
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(3, 130);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(204, 29);
+            this.label4.Size = new System.Drawing.Size(168, 25);
             this.label4.TabIndex = 2;
             this.label4.Text = "Nhập lại mật khẩu";
             // 
@@ -124,10 +130,10 @@ namespace DangKy
             // 
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(3, 188);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 190);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(113, 29);
+            this.label5.Size = new System.Drawing.Size(95, 25);
             this.label5.TabIndex = 3;
             this.label5.Text = "Họ và tên";
             // 
@@ -135,10 +141,10 @@ namespace DangKy
             // 
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(3, 246);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 248);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(62, 29);
+            this.label6.Size = new System.Drawing.Size(51, 25);
             this.label6.TabIndex = 4;
             this.label6.Text = "Tuổi";
             // 
@@ -146,10 +152,10 @@ namespace DangKy
             // 
             this.label7.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(3, 304);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 306);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(121, 29);
+            this.label7.Size = new System.Drawing.Size(102, 25);
             this.label7.TabIndex = 5;
             this.label7.Text = "Số CMND";
             // 
@@ -157,10 +163,10 @@ namespace DangKy
             // 
             this.label8.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(3, 363);
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 365);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(154, 29);
+            this.label8.Size = new System.Drawing.Size(126, 25);
             this.label8.TabIndex = 6;
             this.label8.Text = "Số điện thoại";
             // 
@@ -168,19 +174,19 @@ namespace DangKy
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(243, 12);
+            this.textBox1.Location = new System.Drawing.Point(203, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(554, 34);
+            this.textBox1.Size = new System.Drawing.Size(552, 34);
             this.textBox1.TabIndex = 7;
             // 
             // textBox2
             // 
             this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(243, 70);
-            this.textBox2.MaxLength = 20;
+            this.textBox2.Location = new System.Drawing.Point(203, 68);
+            this.textBox2.MaxLength = 6;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(554, 34);
+            this.textBox2.Size = new System.Drawing.Size(552, 34);
             this.textBox2.TabIndex = 8;
             this.textBox2.UseSystemPasswordChar = true;
             // 
@@ -188,9 +194,10 @@ namespace DangKy
             // 
             this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(243, 128);
+            this.textBox3.Location = new System.Drawing.Point(203, 126);
+            this.textBox3.MaxLength = 6;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(554, 34);
+            this.textBox3.Size = new System.Drawing.Size(552, 34);
             this.textBox3.TabIndex = 9;
             this.textBox3.UseSystemPasswordChar = true;
             // 
@@ -198,39 +205,39 @@ namespace DangKy
             // 
             this.textBox4.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(243, 186);
+            this.textBox4.Location = new System.Drawing.Point(203, 186);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(554, 34);
+            this.textBox4.Size = new System.Drawing.Size(552, 34);
             this.textBox4.TabIndex = 10;
             // 
             // textBox5
             // 
             this.textBox5.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(243, 244);
+            this.textBox5.Location = new System.Drawing.Point(203, 244);
             this.textBox5.MaxLength = 200;
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(554, 34);
+            this.textBox5.Size = new System.Drawing.Size(552, 34);
             this.textBox5.TabIndex = 11;
             // 
             // textBox6
             // 
             this.textBox6.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(243, 302);
+            this.textBox6.Location = new System.Drawing.Point(203, 302);
             this.textBox6.MaxLength = 9;
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(554, 34);
+            this.textBox6.Size = new System.Drawing.Size(552, 34);
             this.textBox6.TabIndex = 12;
             // 
             // textBox7
             // 
             this.textBox7.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox7.Location = new System.Drawing.Point(243, 361);
+            this.textBox7.Location = new System.Drawing.Point(203, 361);
             this.textBox7.MaxLength = 10;
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(554, 34);
+            this.textBox7.Size = new System.Drawing.Size(552, 34);
             this.textBox7.TabIndex = 13;
             // 
             // tableLayoutPanel3
@@ -240,7 +247,7 @@ namespace DangKy
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.25735F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.74265F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 161F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 201F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 202F));
             this.tableLayoutPanel3.Controls.Add(this.button1, 1, 0);
             this.tableLayoutPanel3.Controls.Add(this.button2, 2, 0);
             this.tableLayoutPanel3.Location = new System.Drawing.Point(74, 550);
@@ -255,7 +262,7 @@ namespace DangKy
             this.button1.BackColor = System.Drawing.SystemColors.HighlightText;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(361, 3);
+            this.button1.Location = new System.Drawing.Point(360, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(140, 68);
             this.button1.TabIndex = 0;
@@ -267,7 +274,7 @@ namespace DangKy
             // 
             this.button2.BackColor = System.Drawing.SystemColors.HighlightText;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(551, 3);
+            this.button2.Location = new System.Drawing.Point(550, 3);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(135, 68);
             this.button2.TabIndex = 1;
@@ -298,6 +305,28 @@ namespace DangKy
             this.label1.Size = new System.Drawing.Size(170, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "ĐĂNG KÝ";
+            // 
+            // hidepassword
+            // 
+            this.hidepassword.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.hidepassword.Image = ((System.Drawing.Image)(resources.GetObject("hidepassword.Image")));
+            this.hidepassword.Location = new System.Drawing.Point(761, 68);
+            this.hidepassword.Name = "hidepassword";
+            this.hidepassword.Size = new System.Drawing.Size(55, 34);
+            this.hidepassword.TabIndex = 14;
+            this.hidepassword.UseVisualStyleBackColor = true;
+            this.hidepassword.Click += new System.EventHandler(this.hidepassword_Click);
+            // 
+            // hiderepassword
+            // 
+            this.hiderepassword.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.hiderepassword.Image = ((System.Drawing.Image)(resources.GetObject("hiderepassword.Image")));
+            this.hiderepassword.Location = new System.Drawing.Point(761, 126);
+            this.hiderepassword.Name = "hiderepassword";
+            this.hiderepassword.Size = new System.Drawing.Size(55, 34);
+            this.hiderepassword.TabIndex = 14;
+            this.hiderepassword.UseVisualStyleBackColor = true;
+            this.hiderepassword.Click += new System.EventHandler(this.hiderepassword_Click);
             // 
             // DangKy
             // 
@@ -340,6 +369,8 @@ namespace DangKy
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button hidepassword;
+        private System.Windows.Forms.Button hiderepassword;
     }
 }
 
--- DangKy/DangKy/DangKy.cs
+++ DangKy/DangKy/DangKy.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -30,30 +31,36 @@ namespace DangKy
                 MessageBox.Show("Nhập không đủ thông tin. Vui lòng kiểm tra lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             else
             {
-                if (!SubmitPassCorrect(pass, repass))
-                    MessageBox.Show("Mật khẩu xác nhận không chính xác. Vui lòng kiểm tra lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (pass.Length < 6)
+                    MessageBox.Show("Mật khẩu không đủ mạnh. Vui lòng kiểm tra lại.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 else
                 {
-                    if (!IsValidNumber(age))
-                        MessageBox.Show("Thông tin 'Tuổi' không chính xác. Vui lòng kiểm tra lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    else if (!IsValidNumber(passport) || passport.Length < 9)
-                        MessageBox.Show("Thông tin 'Chứng minh nhân dân ' không chính xác. Vui lòng kiểm tra lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    else if (!IsValidNumber(phone) || phone.Length < 10)
-                        MessageBox.Show("Thông tin 'Số điện thoại' không chính xác. Vui lòng kiểm tra lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if (!SubmitPassCorrect(pass, repass))
+                        MessageBox.Show("Mật khẩu xác nhận không chính xác. Vui lòng kiểm tra lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     else
                     {
-                        if (CheckUserExist(user, pass, fullname, age, passport, phone) == 0)  /// Bị lỗi khi trùng tên đăng nhập
-                            MessageBox.Show("Tên đăng nhập đã tồn tại. Vui lòng kiểm tra lại.");
+                        if (!IsValidNumber(age))
+                            MessageBox.Show("Thông tin 'Tuổi' không chính xác. Vui lòng kiểm tra lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        else if (!IsValidNumber(passport) || passport.Length < 9)
+                            MessageBox.Show("Thông tin 'Chứng minh nhân dân ' không chính xác. Vui lòng kiểm tra lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        else if (!IsValidNumber(phone) || phone.Length < 10)
+                            MessageBox.Show("Thông tin 'Số điện thoại' không chính xác. Vui lòng kiểm tra lại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         else
                         {
-                            MessageBox.Show("Đăng ký thành công !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            textBox1.Clear();
-                            textBox2.Clear();
-                            textBox3.Clear();
-                            textBox4.Clear();
-                            textBox5.Clear();
-                            textBox6.Clear();
-                            textBox7.Clear();
+                            string passCode = ConvertPassword(pass);
+                            if (CheckUserExist(user, passCode, fullname, age, passport, phone) == 0)  /// Bị lỗi khi trùng tên đăng nhập
+                                MessageBox.Show("Tên đăng nhập đã tồn tại. Vui lòng kiểm tra lại.");
+                            else
+                            {
+                                MessageBox.Show("Đăng ký thành công !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                textBox1.Clear();
+                                textBox2.Clear();
+                                textBox3.Clear();
+                                textBox4.Clear();
+                                textBox5.Clear();
+                                textBox6.Clear();
+                                textBox7.Clear();
+                            }
                         }
                     }
                 }
@@ -96,5 +103,49 @@ namespace DangKy
         {
             this.Close();
         }
+        string ConvertPassword(string pass)
+        {
+            SHA256 sh = SHA256.Create();
+            HashAlgorithm hash = HashAlgorithm.Create();
+            byte[] code = hash.ComputeHash(Encoding.UTF8.GetBytes(pass));
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in code)
+                sb.Append(item.ToString("x"));
+            return sb.ToString();
+        }
+        bool showpass = true;
+        bool showrepass = true;
+        private void hidepassword_Click(object sender, EventArgs e)
+        {
+            if (!showpass)
+            {
+                showpass = true;
+                textBox2.UseSystemPasswordChar = true;
+                hidepassword.Image = Image.FromFile(@"E:\Projects\Lập trình trực quan\Phần mềm\Img\hide.png");
+            }
+            else
+            {
+                showpass = false;
+                textBox2.UseSystemPasswordChar = false;
+                hidepassword.Image = Image.FromFile(@"E:\Projects\Lập trình trực quan\Phần mềm\Img\show.png");
+            }
+        }
+
+        private void hiderepassword_Click(object sender, EventArgs e)
+        {
+            if (!showrepass)
+            {
+                showrepass = true;
+                textBox3.UseSystemPasswordChar = true;
+                hiderepassword.Image = Image.FromFile(@"E:\Projects\Lập trình trực quan\Phần mềm\Img\hide.png");
+            }
+            else
+            {
+                showrepass = false;
+                textBox3.UseSystemPasswordChar = false;
+                hiderepassword.Image = Image.FromFile(@"E:\Projects\Lập trình trực quan\Phần mềm\Img\show.png");
+            }
+        }
+
     }
 }
