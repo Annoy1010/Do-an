494cc4c2a94866e5638d0a9a8a0e093c33d2decf
HoangQuan74 <quannguyennhathoang16042002@gmail.com>
2021-11-14 23:05:38 +0700

    Thong tin ca nhan

---
+++ GiaoDien_ThongTinUser/GiaoDien_ThongTinUser/DAO/DaTaProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiaoDien_ThongTinUser.DAO
+{
+    class DataProvider
+    {
+        private string connectionSTR = @"Data Source=.\sqlexpress;Initial Catalog=Library;Integrated Security=True";
+        public DataTable ExcuteQuery(string query)
+        {
+            SqlConnection connection = new SqlConnection(connectionSTR);
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            DataTable data = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            adapter.Fill(data);
+            connection.Close();
+            return data;
+        }
+
+
+    }
+}
---
+++ GiaoDien_ThongTinUser/GiaoDien_ThongTinUser/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GiaoDien_ThongTinUser.DAO;
+
+namespace GiaoDien_ThongTinUser
+{
+    public partial class Form1 : Form
+    {
+        public Form1(string username, string password)
+        {
+            InitializeComponent();
+            LoadInfo(username, password);
+        }
+        void LoadInfo(string username, string password)
+        {
+            DataProvider provider = new DataProvider();
+            string query = "select hoten, tuoi, cmnd, sdt from useraccount where tendn = '" + username + "'";
+            DataTable data = provider.ExcuteQuery(query);
+            txtHoTen.Text = data.Rows[0]["HOTEN"].ToString();
+            txtTuoi.Text = data.Rows[0]["TUOI"].ToString();
+            txtCMND.Text = data.Rows[0]["CMND"].ToString();
+            txtDT.Text = data.Rows[0]["SDT"].ToString();
+        }   
+    }
+}
